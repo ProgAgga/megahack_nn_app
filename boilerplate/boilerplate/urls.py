fc38d1079f91fb8c10dda05251a3f799d79f1354
Igor Beschastnov <beschastnovigor@gmail.com>
2019-04-12 22:55:27 +0300

    Add fundamentals for views.

--- boilerplate/boilerplate/urls.py
+++ boilerplate/boilerplate/urls.py
@@ -4,5 +4,13 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
+
+urlpatterns.extend([
+    path('api/offers', ),
+    path('api/options', ),
+    path('api/sources', ),
+    path('api/dealers', ),
+    path('api/clients', )
+])
---
+++ boilerplate/boilerplate/views.py
@@ -0,0 +1,3 @@
+from rest_framework import generics as gr
+
+from boilerplate.models import *
