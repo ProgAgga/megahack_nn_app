aa6db27ddd80971ebe46e2170bcd96591cfd5796
KovalevVasilii <minskij10@yandex.ru>
2019-04-12 23:23:39 +0300

    Fix fill_db command

--- boilerplate/boilerplate/management/commands/fill_db.py
+++ boilerplate/boilerplate/management/commands/fill_db.py
@@ -6,10 +6,10 @@ import random
 import string
 
 
-class FillDb(BaseCommand):
+class Command(BaseCommand):
     list_of_names = ['Вася', 'Петя', 'Андрей', 'Вова', 'Игорь', 'Никита', 'Вера', 'Надежда']
     list_of_last_names = ['Иванов', 'Петров', 'Сидоров', 'Ульянов', 'Бесчастнов', 'Ковалев', 'Трубов']
-    list_of_sex = ['Муж', 'Жен', 'Не опр']
+    list_of_sex = ['M', 'F', 'NB', None]
     list_of_ages = [19, 23, 26, 56, 92, 14]
 
     @staticmethod
@@ -21,7 +21,7 @@ class FillDb(BaseCommand):
 
     @staticmethod
     def number_generator():
-        return '920'.join(random.choice(string.digits) for _ in range(7))
+        return '920' + ''.join(random.choice(string.digits) for _ in range(7))
 
     def handle(self, *args, **options):
         list_of_dates = list(itertools.islice(self.date_generator(), 10))
@@ -30,5 +30,6 @@ class FillDb(BaseCommand):
             sex = random.choice(self.list_of_sex)
             age = random.choice(self.list_of_ages)
             date = random.choice(list_of_dates)
-
-            Client(name=name, sex=sex, age=age, date=date)
+            phone = self.number_generator()
+            print(phone)
+            Client(name=name, sex=sex, age=age, date_of_admission=date, phone=phone).save()
