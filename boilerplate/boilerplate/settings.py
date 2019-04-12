d710d308642c7fc1df87e77924a67ee97ffe1f8b
Igor Beschastnov <beschastnovigor@gmail.com>
2019-04-12 22:10:00 +0300

    Added db settings.

--- boilerplate/boilerplate/settings.py
+++ boilerplate/boilerplate/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'boilerplate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'main',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
