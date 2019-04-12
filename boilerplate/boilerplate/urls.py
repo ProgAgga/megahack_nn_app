ae48b69d95ba41e0e8b3a11770fa510c4c4b4660
Igor Beschastnov <beschastnovigor@gmail.com>
2019-04-13 00:40:05 +0300

    Add miss reroute.

--- boilerplate/boilerplate/urls.py
+++ boilerplate/boilerplate/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import TemplateView
 
 from boilerplate.views import *
@@ -15,7 +15,9 @@ urlpatterns.extend([
    # path('api/sources', ),
     path('api/dealers', DealerListView.as_view()),
     path('api/clients', ClientsListView.as_view()),
-    path('api/clients/<int:id>', ClientDetailView.as_view())
+    path('api/clients/<int:id>', ClientDetailView.as_view()),
    # path('api/orders', ),
    # path('api/check_offer', )
 ])
+
+urlpatterns.append(re_path('.*', TemplateView.as_view(template_name='index.html')))
