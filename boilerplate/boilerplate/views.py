2a3a17f43725e5a38c3d2a9251a7425b9c3e6a5f
Igor Beschastnov <beschastnovigor@gmail.com>
2019-04-13 17:48:50 +0300

    Add response to check-view

--- boilerplate/boilerplate/views.py
+++ boilerplate/boilerplate/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django import views as vw
+from django.http import HttpResponse
 
 from boilerplate.models import Client, Dealer, Offer
 
@@ -8,8 +9,12 @@ from boilerplate.validations.offer_validation import validate_offer
 
 
 class CheckOffer(vw.View):
+
     def post(self, request):
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
         result = validate_offer(body['client'], body['dealer'], body['offer'])
-
+        return HttpResponse(content=json.dumps({
+            'result': result,
+            **body
+        }))
