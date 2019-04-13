9d447b55b6e4a28754a2161d5e1f205d284d6635
KovalevVasilii <minskij10@yandex.ru>
2019-04-13 20:33:33 +0300

    Add swagger

--- boilerplate/boilerplate/urls.py
+++ boilerplate/boilerplate/urls.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from django.urls import path, re_path
 from django.views.generic import TemplateView
+from kombu.utils import url
+
+from rest_framework.documentation import include_docs_urls
+from rest_framework_swagger.views import get_swagger_view
 
 from boilerplate.detailed_views import *
 from boilerplate.list_views import *
 from boilerplate.views import *
 
+schema_view = get_swagger_view(title="Swagger Docs")
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view),
     path('', TemplateView.as_view(template_name='index.html')),
+
 ]
 
 urlpatterns.extend([
