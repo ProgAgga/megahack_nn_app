d280c170e1dba1c225897abb21644324d10c4a63
Igor Beschastnov <beschastnovigor@gmail.com>
2019-04-14 10:49:32 +0300

    Finish preparations for docker .

--- boilerplate/up.sh
+++ boilerplate/up.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python manage.py add_all
-python manage.py populate_redis
-python manage.py runserver
+python3 manage.py add_all
+python3 manage.py populate_redis
+python3 manage.py runserver 0.0.0.0:8000
