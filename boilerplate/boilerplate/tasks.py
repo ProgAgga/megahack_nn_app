0fc9c4e8c0a22b7c265d0e46805c95a6947fad3b
Kovalev Vasilii <minskij10@yandex.ru>
2019-04-14 07:00:40 +0300

    Add order_offer_validate tsk

--- boilerplate/boilerplate/tasks.py
+++ boilerplate/boilerplate/tasks.py
@@ -1,7 +1,22 @@
+import datetime
+
 from boilerplate.celery import app
 from boilerplate.validations.offer_validation import validate_order
+from boilerplate.models import OfferOrder
 
 
 @app.task(serializer='json')
 def order_offer_validate(order_id):
-    pass
+    offer_order = OfferOrder.objects.filter(id=order_id).first()
+
+    if not offer_order:
+        return
+
+    offer_order.status, valid, invalid = validate_order(offer_order.client.id,
+                                                        offer_order.dealer.id,
+                                                        offer_order.offer.id)
+
+    offer_order.date_processed = datetime.datetime.now()
+    offer_order.save()
+
+
